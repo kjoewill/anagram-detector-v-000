551bff56faa4b086006ce1398796827fe4a12f45
Kevin W <kjoewill@gmail.com>
2018-08-27 19:37:50 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,8 +7,15 @@ class Anagram
   
   def match(list)
     
+    list.collect { |w| isMatch(w) }
+    
   end
   
+  private
+  
+  def isMatch(potential_match)
+    word.sort == potential_match.sort
+  end
   
   
 end
